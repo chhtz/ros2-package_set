c6852b1a9ffb060f46cf564ac7acdfd51e8c32bf
Christoph Hertzberg <christoph.hertzberg@dfki.de>
2026-06-18 18:53:03 +0200

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-puts "\n\n    PULL REQUEST TEST2 \n\n"
+puts "\n\n    PULL REQUEST TEST2 -- updated \n\n"
 
 require_relative "./lib/colcon_package.rb"
 require_relative "./lib/colcon_import_package.rb"
